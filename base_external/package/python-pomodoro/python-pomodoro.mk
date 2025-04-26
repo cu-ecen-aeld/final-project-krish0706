527a1fcf11d1f098da42058742f12331da81f0ac
kshah <krishsowmilshah@gmail.com>
2025-04-25 21:31:49 -0600

    Adds pomodoro server to buildroot

---
+++ base_external/package/python-pomodoro/python-pomodoro.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+#python-pomodoro
+#
+################################################################################
+
+PYTHON_POMODORO_VERSION = 1.0.0
+PYTHON_POMODORO_SOURCE = $(PYTHON_POMODORO_VERSION).tar.gz
+PYTHON_POMODORO_SITE = "https://github.com/krish0706/final-project-pomodoro-server/archive/refs/tags/"
+PYTHON_POMODORO_DEPENDENCIES = python-flask 
+PYTHON_POMODORO_SETUP_TYPE = setuptools
+
+$(eval $(python-package))
