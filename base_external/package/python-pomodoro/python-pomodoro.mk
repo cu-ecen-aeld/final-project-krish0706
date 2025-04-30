977f456fafa2aff3546afd8f22a816ea500e9508
kshah <krishsowmilshah@gmail.com>
2025-04-30 13:44:58 -0600

    Updates pomodoro package to 2.0.0

--- base_external/package/python-pomodoro/python-pomodoro.mk
+++ base_external/package/python-pomodoro/python-pomodoro.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_POMODORO_VERSION = 1.0.0
+PYTHON_POMODORO_VERSION = 2.0.0 
 PYTHON_POMODORO_SOURCE = $(PYTHON_POMODORO_VERSION).tar.gz
 PYTHON_POMODORO_SITE = "https://github.com/krish0706/final-project-pomodoro-server/archive/refs/tags/"
 PYTHON_POMODORO_DEPENDENCIES = python-flask 
